f6e5e9274e6b6f95c7f358269e6d026a83ed074c
Haroun <hrnabd@hotmail.com>
2022-06-29 21:35:26 +0200

    Etape 3

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,40 @@
-fetch('http://localhost:3000/api/products')
+fetch('http://localhost:3000/api/products') // 1. On fetch les données
   .then((res) => res.json())
-  .then((data) => productData(data));
+  .then((data) => {addProduct(data);
+  }); // 2. puis on les passe à la fonction addProduct
 
-const productData = function (data) {
-  let a = document.createElement('a');
-  const IMAGEURL = data[1].imageUrl;
-  a.href = IMAGEURL;
-  a.text = 'first sofa';
-  const items = document.querySelector('#items');
-  items.appendChild(a);
+function addProduct(data) {
+  for (let i = 0; i < data.length; i++) {  // 3. qui grâce à une boucle, va reproduire le template dans le HTML en utilisant les données de l'API 
+    
+    //link
+    const id = data[i]._id; 
+    const a = document.createElement('a');
+    a.href = `./product.html?id=${id}`;
+    document.querySelector('#items').appendChild(a)
+
+    //article
+    const  article = document.createElement('article');
+    document.querySelector(`#items a:nth-child(${i + 1})`).appendChild(article)
+
+    //image
+    const img = document.createElement('img');
+    img.src = data[i].imageUrl;
+    img.alt = data[i].altTxt;
+    document.querySelector(`#items a:nth-child(${i + 1}) article`).appendChild(img)
+
+    //h3
+    const productName = data[i].name;
+    const h3 = document.createElement('h3');
+    h3.textContent = productName;
+    h3.classList.add('productName');
+    document.querySelector(`#items a:nth-child(${i + 1}) article`).appendChild(h3)
+
+    //p
+    const productDescription = data[i].description;
+    const p = document.createElement('p');
+    p.textContent = productDescription;
+    p.classList.add('productDescription');
+    document.querySelector(`#items a:nth-child(${i + 1}) article`).appendChild(p)
+  }
 };
+
