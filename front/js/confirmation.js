53c03591bc9e5e5dc112bd853203178f9addf83c
Haroun <hrnabd@hotmail.com>
2022-09-05 15:24:57 +0200

    petite mise à jour

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -4,6 +4,7 @@ const orderId = new URLSearchParams(window.location.search).get('id');
 // Insérer l'id dans la page confirmation
 document.querySelector('#orderId').innerText = orderId;
 
+//Remerciement
 const thanks = document.createElement('span');
-thanks.innerText = 'Merci pour votre confiance et à bientôt !';
+thanks.innerHTML = '</br>Merci pour votre confiance et à bientôt !';
 document.querySelector('.confirmation p').appendChild(thanks);
